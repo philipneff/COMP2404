35c56db57e35cc59763254a6ff3d7f5f6de60cb4
Neff, Phillip A <phillip.neff@rockwellcollins.com>
2018-11-13 13:21:24 -0500

    print statements before array change

--- t04/Array.cc
+++ t04/Array.cc
@@ -9,14 +9,19 @@ Array::~Array(){}
 
 void Array::add(Book& newBook)
 {
+    std::cout<<"here1"<<endl;
     elements[size]=newBook;
+    std::cout<<"here2"<<endl;
     size+=1;
+
 
 }
 
 void Array::print()
 {
+    std::cout<<"there"<<endl;
     for(int i=0; i<size;i++){
         elements[i].print();
+
     }
 }
